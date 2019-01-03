e70338644c6fea776c260bc3cdfa0134ee0f7134
wktan <tanweikang_96@hotmail.com>
2019-01-03 19:58:47 +0000

    added the forms and function to update profile (change profile image, username etc)

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.db import transaction
-from .models import User, Developer, Clinician
+from .models import User, Developer, Clinician, Profile
 
 
 class DeveloperRegisterForm(UserCreationForm):
@@ -34,3 +34,17 @@ class ClinicianRegisterForm(UserCreationForm):
             user.save()
         Clinician.objects.create(user=user)
         return user
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/views.py
+++ users/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import CreateView, TemplateView
 from django.shortcuts import redirect
+from django.contrib import messages
 from .models import User
-from .forms import DeveloperRegisterForm
-from .forms import ClinicianRegisterForm
+from .forms import DeveloperRegisterForm, ClinicianRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 # from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
@@ -19,10 +19,24 @@ def testrestricted(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your profile has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
 
 
-# Create your views here.
 class RegisterView(TemplateView):
     template_name = "users/register.html"
 
