2add67cc832cdaac90fb2f853721ce7d09c3e0bb
wktan <tanweikang_96@hotmail.com>
2019-01-03 16:02:26 +0000

    edited forms to make email a compulsory field though username is still used to login, possible feature in future to use email to login instead

--- users/forms.py
+++ users/forms.py
@@ -5,25 +5,32 @@ from .models import User, Developer, Clinician
 
 
 class DeveloperRegisterForm(UserCreationForm):
+    email = forms.EmailField() # required in order to make it a compulsory field
+
     class Meta(UserCreationForm.Meta):
         model = User
+        fields = ['username', 'email', 'password1', 'password2']  # included to make email appear before passwords (more intuitive)
 
     @transaction.atomic
     def save(self):
         user = super().save(commit=False)  # commit set to False to return object not saved to db yet
         user.is_developer = True
         user.save()
-        developer = Developer.objects.create(user=user)
+        Developer.objects.create(user=user)  # creates developer based on user instance
         return user
 
 
 class ClinicianRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
     class Meta(UserCreationForm.Meta):
         model = User
+        fields = ['username', 'email', 'password1', 'password2']
 
     def save(self, commit=True):
         user = super().save(commit=False)
         user.is_clinician = True
         if commit:
             user.save()
+        Clinician.objects.create(user=user)
         return user
