4a18f1ec3d4074155f457cb0cd9fb838b6deb6c0
wktan <tanweikang_96@hotmail.com>
2019-01-04 17:58:29 +0000

    fixed the problem the html file needs to be under a directory with the same app name under templates directory

--- nhs_pg/urls.py
+++ nhs_pg/urls.py
@@ -18,8 +18,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-# from users import views as user_views
-from . import views
+from users import views as user_views
+# from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
