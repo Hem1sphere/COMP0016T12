56832c60e423c811e3a62b581cca6b15a4b2848e
wktan <tanweikang_96@hotmail.com>
2019-03-04 00:39:43 +0000

    changed urls.py

--- nhs_pg/urls.py
+++ nhs_pg/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from users import views as user_views
+from django.views.static import serve
 # from . import views
 
 urlpatterns = [
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('challenges/', include('challenges.urls')),
