c32a300b10b3fbfbded8068ba4a4af9101fd7ac0
wktan <tanweikang_96@hotmail.com>
2019-03-05 02:48:08 +0000

    added database settings

--- nhs_pg/settings.py
+++ nhs_pg/settings.py
@@ -81,8 +81,15 @@ WSGI_APPLICATION = 'nhs_pg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'sql_server.pyodbc',
+        'HOST': 'nhsplayground.database.windows.net',
+        'NAME': 'nhsplaygrounddatabase',
+        "PORT": '',
+        'USER': 'nhsplayground_admin@nhsplayground',
+        'PASSWORD': 'hello123!',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        }
     }
 }
 
