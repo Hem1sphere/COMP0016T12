36ea9e2ee1c8b49a391f2cd3c999a4a32af530dc
Hem1sphere <fitz0126@hotmail.com>
2019-03-06 17:46:04 +0000

    Reformatted toolbar

--- nhs_pg/settings.py
+++ nhs_pg/settings.py
@@ -139,6 +139,10 @@ CKEDITOR_IMAGE_BACKEND = "pillow"
 
 CKEDITOR_CONFIGS = {
     'default': {
-        'toolbar': None,
+        'toolbar': [["Format", "Bold", "Italic", "Underline", "Strike", "SpellChecker"],
+                ['NumberedList', 'BulletedList', "Indent", "Outdent", 'JustifyLeft', 'JustifyCenter',
+                 'JustifyRight', 'JustifyBlock'],
+                ["Image", "Table", "Link", "Unlink", "Anchor", "SectionLink", "Subscript", "Superscript"], ['Undo', 'Redo'], ["Source"],
+                ["Maximize"]],
     },
 }
